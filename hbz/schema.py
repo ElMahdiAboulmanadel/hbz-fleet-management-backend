8a1979dfe75f2dd7113c4442af03b090d09f4a71
El Mahdi Aboulmanadel <mahdi@allrythm.com>
2022-09-28 23:24:39 +0100

    Added token revoking mutation

--- hbz/schema.py
+++ hbz/schema.py
@@ -9,5 +9,6 @@ class Mutation(api.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     verify_token = graphql_jwt.Verify.Field()
+    revoke_token = graphql_jwt.Revoke.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
